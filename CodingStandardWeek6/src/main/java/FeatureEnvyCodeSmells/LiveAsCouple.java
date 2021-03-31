13d155eecdeaec0bc50c5ad0367c15f6ad478862
paperMountainn <zishan_pan@mymail.sutd.edu.sg>
2021-03-31 23:47:02 +0800

    add more w6 codesmells code

---
+++ CodingStandardWeek6/src/main/java/FeatureEnvyCodeSmells/LiveAsCouple.java
@@ -0,0 +1,24 @@
+package FeatureEnvyCodeSmells;
+
+public class LiveAsCouple {
+	Project[] ourProject;
+
+	public LiveAsCouple(Project myProject, Project herProject) {
+		this.ourProject[0] = myProject;
+		this.ourProject[1] = herProject;
+	}
+	
+	public Project[] getProject() {
+		return this.ourProject;
+	}
+	
+	// sudiptac: work as a team
+	public void doProject() {
+		workOnProject(ourProject[0]);
+		workOnProject(ourProject[1]);
+	}
+	
+	public void workOnProject(Project p) {
+		//some code here
+	}
+}
---
+++ CodingStandardWeek6/src/main/java/LongMethodSmells/myClass.java
@@ -0,0 +1,7 @@
+package LongMethodSmells;
+
+public class myClass {
+    public static void main(String[] args){
+        System.out.println("he");
+    }
+}
